25b2ed9c6751e89b849cf1ac044871a46a49e509
Niklas Metz <niklasmetz@gmx.de>
2021-12-10 15:00:04 +0100

    Rewrote some parts of the quantum variational circuit.

--- QuantumVariationalKernel.py
+++ QuantumVariationalKernel.py
@@ -30,79 +30,66 @@ class QuantumVariationalKernel():
     def kernel_with_params(self, x1, x2, params):
         return 1-self.qnode(x1, x2, params)[0]
 
-    def train(self, X, Y=None):
-        opt = qml.GradientDescentOptimizer(0.2)
-
-        for i in range(1000):
-            print(i)
-            subset = np.random.choice(list(range(len(X))), 5)
-            if self.cost_func == "KTA":
-                #Semi-supervised learning with the kernel target alignment
-                cost = lambda _params: -qml.kernels.target_alignment(X[subset], Y[subset], lambda x1, x2: self.kernel_with_params(x1, x2, _params),assume_normalized_kernel=True)
-            elif self.cost_func == "daviesbouldin":
-                kmeans = KernelKMeans(n_clusters=2, max_iter=100, random_state=0, verbose=1, kernel=self.kernel)
-                cost = lambda _params: metrics.davies_bouldin_score(
-                    X[subset],
-                    kmeans.fit(X[subset], kernel=lambda x1, x2: self.kernel_with_params(x1, x2, _params)).labels_
-                )
-            elif self.cost_func == "tripletloss":
-                kmeans = KernelKMeans(n_clusters=2, max_iter=100, random_state=0, verbose=1, kernel=self.kernel)
-                la = kmeans.fit(X[subset], self.kernel).labels_
-
-                cost = lambda _params: CostFunctions.triplet_loss(X[subset], la, 1, 0.5, lambda x1, x2: self.kernel_with_params(x1, x2, _params))
-            else:
-                kmeans = KernelKMeans(n_clusters=2, max_iter=100, random_state=0, verbose=1, kernel=self.kernel)
-
-                la = kmeans.fit(X[subset], self.kernel).labels_
-                la = np.array([y if y == 1 else -1 for y in la])
-                cost = lambda _params: -qml.kernels.target_alignment(
-                    X[subset],
-                    la,
-                    lambda x1, x2: self.kernel_with_params(x1, x2, _params),
-                    assume_normalized_kernel=True,
-                )
-
-            # Optimization step
-            self.params= opt.step(cost, self.params)
-
-            # Output the current quality
-            if (i + 1) % 50 == 0:
-                km = KernelKMeans(n_clusters=2, max_iter=100, random_state=0, verbose=1, kernel=self.kernel)
-                lab = km.fit(X, self.kernel).labels_
-                print("{}, {}".format(lab, metrics.davies_bouldin_score(X, lab)))
-
-    def semi_supervised(self, X, Y, val_size=5,epochs=500):
-        opt = qml.GradientDescentOptimizer(0.01)
+    def train(self, X, Y=None, train_size=20, batch_size=5, val_size=5, epochs=500):
+        opt = qml.GradientDescentOptimizer(0.05)
 
         num_samples = X.shape[0]
-        train_X, train_Y = X[:num_samples-val_size], Y[:num_samples-val_size]
-        val_X, val_Y = X[num_samples-val_size:], Y[num_samples-val_size:]
+        if train_size+val_size > num_samples:
+            raise ValueError("Provided {} samples, but required for train and validation set are {} samples.".format(num_samples, train_size+val_size))
+
+        train_X, val_X = X[:train_size], X[train_size:train_size+val_size]
+        if Y is not None:
+            #Unsupervised -> no Y
+            train_Y, val_Y = Y[:train_size], Y[train_size:train_size+val_size]
 
         for epoch in range(epochs):
             print("Epoch: {}".format(epoch))
             perm = np.random.permutation(train_X.shape[0])
-            parts = [perm[i::10] for i in range(10)]
-            for idx, val in enumerate(parts):
-                cost = lambda _params: -qml.kernels.target_alignment(train_X[val], train_Y[val], lambda x1, x2: self.kernel_with_params(x1, x2, _params))
-                self.params, c = opt.step_and_cost(cost, self.params)
-                c_val = qml.kernels.target_alignment(val_X, val_Y, self.kernel)
-                print("Step: {}, cost: {}, val_cost: {}".format((epoch*10)+(idx), -c, c_val))
+            batches = int(train_size/batch_size)
+            parts = [perm[i::batches] for i in range(batches)]
+
+            for idx, subset in enumerate(parts):
+                if self.cost_func == "KTA-supervised":
+                    cost = lambda _params: -qml.kernels.target_alignment(train_X[subset], train_Y[subset], lambda x1, x2: self.kernel_with_params(x1, x2, _params))
+                    self.params, c = opt.step_and_cost(cost, self.params)
+                    cost_val = qml.kernels.target_alignment(val_X, val_Y, self.kernel)
+                    print("Step: {}, cost: {}, val_cost: {}".format(epoch*batches+idx, c, cost_val))
+                elif self.cost_func == "triplet-loss-supervised":
+                    cost = lambda _params: CostFunctions.triplet_loss(train_X[subset], train_Y[subset], 0.5, lambda x1, x2: self.kernel_with_params(x1, x2, _params))
+                elif self.cost_func == "davies-bouldin":
+                    kmeans = KernelKMeans(n_clusters=2, max_iter=100, random_state=0, verbose=1, kernel=self.kernel)
+                    cost = lambda _params: metrics.davies_bouldin_score(
+                        train_X[subset],
+                        kmeans.fit(train_X[subset], kernel=lambda x1, x2: self.kernel_with_params(x1, x2, _params)).labels_
+                    )
+                    self.params, c = opt.step_and_cost(cost, self.params)
+                elif self.cost_func == "calinski-harabasz":
+                    pass
+                elif self.cost_func == "KTA-unsupervised":
+                    pass
+                elif self.cost_func == "triplet-loss-unsupervised":
+                    pass
+                else:
+                    pass
+
             if (epoch + 1) % 100 == 0:
                 km = KernelKMeans(n_clusters=2, max_iter=100, random_state=0, verbose=1, kernel=self.kernel)
                 lab = km.fit(X, self.kernel).labels_
-                print("Epoch {}: {}".format(epoch, metrics.davies_bouldin_score(X, lab)))
+                davies = metrics.davies_bouldin_score(X, lab)
+                calinski = metrics.calinski_harabasz_score(X, lab)
+                print("Clustering test! Epoch {}: davies-bouldin: {}, calinski-harabasz: {}".format(epoch, davies, calinski))
 
 
 if __name__ == '__main__':
     from datasets.iris import iris
-    data = iris(train_size=55, test_size=0, shuffle=True)
+    data = iris(train_size=50, test_size=0, shuffle=True)
     Y_new = np.array([y if y == 1 else -1 for y in data.train_target])
 
     wires = 4
     layers = 3
 
     init_params = random_params(num_wires=wires, num_layers=layers, params_per_wire=2)
-    qvk = QuantumVariationalKernel(wires=wires, ansatz=ansatz2, init_params=init_params, cost="tripletloss")
+    qvk = QuantumVariationalKernel(wires=wires, ansatz=ansatz2, init_params=init_params, cost="KTA-supervised")
 
     #qvk.train(X, Y_new)
-    qvk.semi_supervised(data.train_data, Y_new)
+    qvk.train(data.train_data, Y_new)
