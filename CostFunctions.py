bdf6f6d882761f0509162efc516b04dbce8b4c02
Niklas Metz <niklasmetz@gmx.de>
2021-12-14 11:10:09 +0100

    Added multiclass kernel target alignment.

--- CostFunctions.py
+++ CostFunctions.py
@@ -2,14 +2,13 @@ from pennylane import numpy as np
 import pennylane as qml
 import random
 
-def target_alignment(
+def multiclass_target_alignment(
     X,
     Y,
     kernel,
+    num_classes,
     assume_normalized_kernel=False,
-    rescale_class_labels=True,
 ):
-    """Kernel-target alignment between kernel and labels."""
 
     K = qml.kernels.square_kernel_matrix(
         X,
@@ -17,27 +16,19 @@ def target_alignment(
         assume_normalized_kernel=assume_normalized_kernel,
     )
 
-    if rescale_class_labels:
-        nplus = np.count_nonzero(np.array(Y) == 1)
-        nminus = len(Y) - nplus
-        _Y = np.array([y / nplus if y == 1 else y / nminus for y in Y])
-    else:
-        _Y = np.array(Y)
+    num_samples = Y.shape[0]
+    T = [[1 if Y[0] == Y[j] else (-1/(num_classes-1)) for j in range(num_samples)]]
+    if num_samples.shape[0] > 1:
+        for i in range(1, num_samples):
+            T = np.concatenate((T, [[1. if Y[i] == Y[j] else (-1/(num_classes-1)) for j in range(num_samples)]]), axis=0)
 
-    T = np.outer(_Y, _Y)
-    inner_product = np.sum(K * T)
-    norm = np.sqrt(np.sum(K * K) * np.sum(T * T))
-    inner_product = inner_product / norm
+    return qml.math.frobenius_inner_product(K, T, normalize=True)
 
-    return inner_product
-
-def triplet_loss(X, labels, num_samples, alpha, dist_func):
+def triplet_loss(X, labels, alpha, dist_func):
     sum = 0.0
-    for i in range(num_samples):
-        anchor_index = random.randrange(labels.shape[0])
-        anchor = X[anchor_index]
-        negative = random.choice(X[labels != labels[anchor_index]])
-        positive_mask = labels == labels[anchor_index]
+    for anchor_idx, anchor in enumerate(X):
+        negative = random.choice(X[labels != labels[anchor_idx]])
+        positive_mask = labels == labels[anchor_idx]
         #positive_mask = vector_change(positive_mask, False, anchor_index)
         positive = random.choice(X[positive_mask])
         sum = sum + max(dist_func(anchor, positive) - dist_func(anchor, negative) + alpha, 0)
