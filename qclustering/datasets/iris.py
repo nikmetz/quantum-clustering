49ce70e44fedc9460a8f3f4d25423039189fdbdb
Niklas Metz <niklasmetz@gmx.de>
2022-01-13 14:05:19 +0100

    Iris can return all 150 samples as test data now.

--- qclustering/datasets/iris.py
+++ qclustering/datasets/iris.py
@@ -5,8 +5,8 @@ from qclustering.datasets.utils import DataSet
 MAX_SAMPLES = 150
 
 def iris(train_size=95, val_size=5, test_size=50, shuffle=True, classes=[0,1,2]) -> DataSet:
-    if train_size + test_size + val_size > MAX_SAMPLES:
-        raise ValueError("Requested too many samples from the iris dataset. Maximum is: {}, but {} were requested.".format(MAX_SAMPLES, train_size + test_size + val_size))
+    #if train_size + test_size + val_size > MAX_SAMPLES:
+    #    raise ValueError("Requested too many samples from the iris dataset. Maximum is: {}, but {} were requested.".format(MAX_SAMPLES, train_size + test_size + val_size))
     data, target = datasets.load_iris(return_X_y=True)
     if len(classes) < 3:
         if len(classes) * 50 > 100:
@@ -20,6 +20,9 @@ def iris(train_size=95, val_size=5, test_size=50, shuffle=True, classes=[0,1,2])
 
     x_train, y_train = data[:train_size], target[:train_size]
     x_val, y_val = data[train_size:train_size+val_size], target[train_size:train_size+val_size]
-    x_test, y_test = data[train_size+val_size:train_size+val_size+test_size], target[train_size+val_size:train_size+val_size+test_size]
+    if train_size + test_size + val_size > MAX_SAMPLES:
+        x_test, y_test = data[:test_size], target[:test_size]
+    else:
+        x_test, y_test = data[train_size+val_size:train_size+val_size+test_size], target[train_size+val_size:train_size+val_size+test_size]
 
     return DataSet(x_train, y_train, x_val, y_val, x_test, y_test)
